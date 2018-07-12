d973e96d1bc13f0d698775c1ad3a98a48af998aa
ags1773 <amogh1773@gmail.com>
2018-07-12 22:48:06 +0530

    comments routes working

--- api/controllers/comments_controllers.js
+++ api/controllers/comments_controllers.js
@@ -21,35 +21,11 @@ exports.comments_get_all = (req, res) => {
     .catch(err => res.status(500).json({ error: err }))
 }
 exports.comments_create_comment = (req, res) => {
-  // // find post; add comment to it
-
-  // Posts.findById(req.params.postId)
-  //   .exec()
-  //   .then(foundPost => {
-  //     if (!foundPost) return res.status(404).json({ message: `Post with ID ${req.params.postId} not found!` })
-  //     const newComment = new Comments({
-  //       author: req.body.author,
-  //       comment: req.body.comment
-  //     })
-  //     return Comments.create(newComment)
-  //   })
-  //   .then(obj => {
-  //     res.status(200).json({
-  //       newMessage: obj
-  //     })
-  //     // obj.b.comments.push(obj.a)
-  //     // return obj.b.save()
-  //   })
-  //   .then((result) => {
-  //     res.status(201).json({
-  //       message: 'Comment saved',
-  //       result: result
-  //     })
-  //   })
-  //   .catch(err => res.status(500).json({ error: err }))
+  // // find post, if exists, create new comment and push it into the post
 
   Posts.findById(req.params.postId, (err, foundPost) => {
-    if (err) res.status(500).json({ error: err })
+    if (err) return res.status(500).json({ error: err })
+    if (!foundPost) return res.status(404).json({ message: 'Post not found!' })
     const newComment = new Comments({
       author: req.body.author,
       comment: req.body.comment
@@ -65,99 +41,71 @@ exports.comments_create_comment = (req, res) => {
       })
     })
   })
+
+  // Posts.findById(req.params.postId)
+  //   .exec()
+  //   .then(post => {
+  //     if (!post) throw new Error('Post not found!')
+  //     const newComment = new Comments({
+  //       author: req.body.author,
+  //       comment: req.body.comment
+  //     })
+  //     return Comments.create(newComment) // returns promise
+  //   })
+  //   .then(createdComment => {
+  //     post.comments.push(createdComment)
+  //     return post.save() // returns promise
+  //   })
+  //   .then(saved => {
+  //     res.status(201).json({ message: 'Successfully created new comment' })
+  //   })
+  //   .catch(err => {
+  //     res.status(500).json({ error: err.message })
+  //   })
+}
+
+exports.comments_get_one = (req, res) => {
+  // // Find post - If post exists, find if given comment is part of it - if yes, return that comment
+
+  Posts.findById(req.params.postId)
+    .exec()
+    .then(post => {
+      if (!post) throw new Error('Post not found!') // how to send status(404) with Error??
+      // if (!post) return res.status(404).json({ message: 'Post not found!' })
+      if (post.comments.indexOf(req.params.commentId) === -1) {
+        throw new Error('Comment not found on given post')
+        // return res.status(404).json({ message: 'Comment not found on given post' })
+      }
+      return Comments.findById(req.params.commentId).exec()
+    })
+    .then(comment => {
+      let formattedComment = {
+        _id: comment._id,
+        author: comment.author,
+        comment: comment.comment
+      }
+      res.status(200).json(formattedComment)
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message })
+    })
 }
 
 exports.comments_delete_comment = (req, res) => {
   Posts.findById(req.params.postId)
-    // .populate('comments')
     .exec()
-    .then(foundPost => {
-      if (!foundPost) return res.status(404).json({ message: `Post with ID ${req.params.postId} not found!` })
-      return Comments.findById(req.params.commentId).exec()
-    })
-    .then(foundComment => {
-      Comments.findByIdAndRemove(foundComment._id).exec()
-    })
-    .then(() => {
-      res.status(200).json({
-        message: 'Comment Deleted successfully!'
-      })
-    })
-    .catch(err => res.status(500).json({ error: err }))
-}
-
-// exports.comments_delete_comment = (req, res) => {
-//   Posts.findById(req.params.postId)
-//     .exec()
-//     .then(foundPost => {
-//       if (foundPost.comments.indexOf(req.params.commentId) === -1) {
-//         res.status(404).json({ error: 'Comment not found for given post' })
-//         return
-//       }
-//       return Comments.findByIdAndRemove(req.params.commentId).exec()
-//     })
-//     .then(() => {
-//       res.status(200).json({
-//         message: 'Comment Deleted'
-//       })
-//     })
-//     .catch(err => res.status(500).json({ error: err }))
-// }
-
-exports.comments_get_one = (req, res) => {
-  // Posts.findById(req.params.postId, (err, foundPost) => {
-  //   if (err) return res.status(500).json({ error: err })
-  //   if (foundPost.comments.indexOf(req.params.commentId) === -1) {
-  //     return res.status(404).json({ error: 'Comment not found for given post' })
-  //   }
-  //   Comments.findById(req.params.commentId, (err, foundComment) => {
-  //     if (err) res.status(500).json({ error: err })
-  //     res.status(200).json(foundComment)
-  //   })
-  // })
-  Posts.findById(req.params.postId)
-    .exec()
-    .then(foundPost => {
-      if (foundPost.comments.indexOf(req.params.commentId) === -1) {
-        // res.status(404).json({ error: 'Comment not found for given post' })
-        throw new Error('Comment not found for given post')
+    .then(post => {
+      if (!post) throw new Error('Post not found!')
+      if (post.comments.indexOf(req.params.commentId) === -1) {
+        throw new Error('Comment not found on given post')
       }
-      return Comments.findById(req.params.commentId).exec().catch(err => console.log(err))
-    }, err => {
-      // console.log(err)
-      res.status(404).json({error: err})
+      return Comments.findByIdAndRemove(req.params.commentId).exec()
     })
-    .then(foundComment => {
-      console.log(' ......', foundComment)
-      if (!foundComment) {
-        throw new Error('Comment not found')
-      }
-      res.status(200).json(foundComment).catch(err => console.log(err))
-    }, err => {
-      // console.log('>>>>>.', err)
-      res.status(404).json({error: err})
+    .then(result => res.status(200).json({ 
+      message: 'Comment deleted successfully!',
+      result: result
+    }))
+    .catch(err => {
+      res.status(500).json({ error: err.message })
     })
-    .catch(err => res.status(404).json({error: err}))
-  // ----------------- DNC --------------------
-  // Posts.findById(req.params.postId)
-  //   .exec()
-  //   .then(foundPost => {
-  //     if (foundPost.comments.indexOf(req.params.commentId) === -1) {
-  //       res.status(404).json({ error: 'Comment not found for given post' })
-  //     }
-  //     throw 'ee '
-  //     return Comments.findById(req.params.commentId).exec().catch(err => console.log(err))
-  //   }, err => console.log(err)
-  //   )
-  //   .then(foundComment => {
-  //     console.log(' ......', foundComment)
-  //     if (!foundComment) {
-  //       res.status(404).json({ error: 'Comment not found' })
-  //       return
-  //     }
-  //     res.status(200).json(foundComment).catch(err => console.log(err))
-  //   }, err => console.log('>>>>>.', err)
-  //   )
-  //   .catch(err => console.log(err))
-  // -------------------------------------------
 }
